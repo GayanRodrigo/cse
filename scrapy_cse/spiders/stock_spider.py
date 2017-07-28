45aa93372d52df75526c3e17716499f7d17ad733
Gayan Rodrigo <madushan.gayan@gmail.com>
2017-07-29 02:46:44 +0530

    scraping all companies in alphabetic order

--- scrapy_cse/spiders/stock_spider.py
+++ scrapy_cse/spiders/stock_spider.py
@@ -2,19 +2,18 @@ import scrapy
 import json
 
 
-class StockSpider(scrapy.Spider):
-    name = "stocks"
-
+class CompanySpider(scrapy.Spider):
+    name = "company"
+    url = 'https://www.cse.lk/api/alphabetical'
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     def start_requests(self):
-        url = 'https://www.cse.lk/api/alphabetical'
-        yield scrapy.FormRequest(url, callback=self.parse, formdata={'alphabet': 'A'})
+        for letter in self.alphabet:
+            yield scrapy.FormRequest(self.url, callback=self.parse, formdata={'alphabet': letter})
 
     def parse(self, response):
         resp = json.loads(response.body)
         for company in resp['reqAlphabetical']:
-            #print company
-            yield company
+            print company['name']
+            #yield company
 
-        '''next_page = response.css('li.next a::attr(href)').extract_first()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)'''
+
