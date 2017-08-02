e8019cf62289b280b52213c87276e4a4a61c085e
grodrigo <grodrigo@virtusapolaris.com>
2017-08-02 13:37:18 +0530

    commiting changes

--- scrapy_cse/spiders/stock_spider.py
+++ scrapy_cse/spiders/stock_spider.py
@@ -1,11 +1,11 @@
 import scrapy
 import json
 
-
 class CompanySpider(scrapy.Spider):
     name = "company"
     url = 'https://www.cse.lk/api/alphabetical'
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	
     def start_requests(self):
         for letter in self.alphabet:
             yield scrapy.FormRequest(self.url, callback=self.parse, formdata={'alphabet': letter})
